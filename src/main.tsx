9f3c39e26764a6cc67e4cc7453fdb179e9f05584
Bogdan <ciborg123@yandex.ru>
2026-01-06 14:52:25 +0300

    fix lab 6

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { store } from './store/store'
 import './index.css'
-import App from './App.tsx'
+import App from './App'
 
 // Регистрация service worker для PWA
 if ('serviceWorker' in navigator) {
--- src/store/store.ts
+++ src/store/store.ts
@@ -5,7 +5,7 @@ export const store = configureStore({
   reducer: {
     filters: filtersReducer,
   },
-  devTools: process.env.NODE_ENV !== 'production',
+  devTools: import.meta.env.DEV,
 });
 
 export type RootState = ReturnType<typeof store.getState>;
